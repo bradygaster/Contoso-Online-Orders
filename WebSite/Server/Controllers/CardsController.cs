17402112e832f1866bb026a393d91768c91f3939
Brady Gaster <bradygaster@Bradys-MacBook-Pro.local>
2020-03-13 23:09:14 -0700

    adding new send-to-shipment card

--- WebSite/Server/Controllers/CardsController.cs
+++ WebSite/Server/Controllers/CardsController.cs
@@ -87,5 +87,62 @@ namespace Server.Controllers
 
             return Ok(json);
         }
+
+        [HttpGet("{orderId}/createSendToShippingCard")]
+        public async Task<ActionResult<string>> CreateSendToShippingCard(int orderId)
+        {
+            var order = await _context.Order.Include(x => x.Items).FirstAsync(x => x.Id == orderId);
+
+            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+
+            // add the heading
+            card.Body.Add(new AdaptiveTextBlock() 
+            {
+                Text = $"Order #{orderId} Received",
+                Size = AdaptiveTextSize.Large,
+                Weight = AdaptiveTextWeight.Bolder
+            });
+
+            // add the call to action
+            card.Body.Add(new AdaptiveTextBlock() 
+            {
+                Text = $"Inventory verified. Please ship:",
+                Weight = AdaptiveTextWeight.Bolder
+            });
+
+            // show the items in the order and their quantities
+            var cartItemFactSet = new AdaptiveFactSet();
+            foreach (var cartItem in order.Items)
+            {
+                var product = _context.Products.Find(cartItem.ProductId);
+                cartItemFactSet.Facts.Add(new AdaptiveFact
+                {
+                    Title = product.Name,
+                    Value = cartItem.Quantity.ToString()
+                });
+            }
+            card.Body.Add(cartItemFactSet);
+
+            // add the choices
+            var choices = new AdaptiveChoiceSetInput() { Id = "ShipmentConfirmed" };
+            choices.Choices.Add(new AdaptiveChoice { Title = "Yes", Value = "true" });
+            choices.Choices.Add(new AdaptiveChoice { Title = "No", Value = "false" });
+            card.Body.Add(choices);
+
+            // add the submit button
+            card.Actions.Add(new AdaptiveSubmitAction
+            {
+                Id = "ShipmentConfirmedOkButton",
+                Title = "OK",
+                Data = new {
+                    OrderId = order.Id
+                }
+            });
+
+            // get the json for the card 
+            var json = card.ToJson();
+
+            return Ok(json);
+        }
     }
 }
